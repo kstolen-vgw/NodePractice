6bb5d7d08a712e3a3de79df56325ac50bc111b11
Kristian Stolen <103400331+kstolen-vgw@users.noreply.github.com>
2022-05-16 14:02:13 +0800

    Working with ticks and asynchronous methods
    I think I need to review async/await further. Calling these within a synchronous method makes some sense, everything is queued for the beginning of the next event loop cycle except for `setTimeout` which gets queued onto the end of the following cycle.
    
    Calling these within an async method appears to make the program wait for each method to execute before proceeding, except for the `setTimeout` method, unless it's contained within an awaited promise and the resolution is in the timeout...

--- SimpleWebServer/index.js
+++ SimpleWebServer/index.js
@@ -40,6 +40,73 @@ app.get('/cars', (req, res) => {
     res.send(cars)
 })
 
+let asyncMethod = async () => "4. Async/await"
+
+app.get('/nexttickasync', async (req, res) => {
+    console.log('')
+
+    //this will execute last
+    setTimeout(() => {
+        console.log("1. setTimeout")
+    }, 0)
+
+    // wait for this to complete
+    process.nextTick(() => {
+        console.log("2. nextTick")
+    })
+
+    // then wait for this
+    new Promise((resolve, reject) => resolve('3. Promise'))
+        .then(console.log)
+    
+    // then wait for this
+    await asyncMethod()
+        .then(console.log)
+
+    await new Promise((resolve, reject) => 
+        setTimeout(() =>    
+            resolve("5. promise timeout", 100)))
+        .then(console.log)
+
+    console.log("6. current tick")
+
+    res.send()
+})
+
+app.get('/nexttick', (req, res) => {
+    console.log('')
+    
+    // execute this at the end of the following tick
+    setTimeout(() => {
+        console.log("1. setTimeout")
+    }, 0)
+
+    // execute this at the beginning of the next tick
+    process.nextTick(() => {
+        console.log("2. nextTick")
+    })
+
+    // queue this after .nextTick
+    new Promise((resolve, reject) => resolve('3. Promise'))
+        .then(console.log)
+
+    // queue this after promise
+    asyncMethod()
+        .then(console.log)
+
+    // queue this after async method
+    new Promise((resolve, reject) =>
+        // queue this after previous setTimeout 
+        setTimeout(() =>    
+            resolve("5. promise timeout", 100)))
+        .then(console.log)
+
+    // execute this now
+    console.log("6. current tick")
+
+    res.send()
+})
+
 const server = app.listen(PORT, HOST)
 
 process.on('SIGTERM', () => {
